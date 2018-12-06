3c0cb42028013f08b92c8a2f8cb79522a883c2c2
Mike Garon <mike@flarejewelry.co>
2018-12-06 11:37:07 -0500

    Show more beacon data when environment ble logging is verbose.

--- src/bits/BeaconCache.js
+++ src/bits/BeaconCache.js
@@ -3,6 +3,7 @@ import {
     BEACON_CACHE_PRUNE_INTERVAL_IN_MS,
     BEACON_CACHE_MAX_AGE_IN_MINS,
     UNIQUE_BEACON_TIMING_IN_MS,
+    BLUETOOTH_BEACON_LOGGING,
 } from '../constants';
 
 export default class BeaconCache {
@@ -52,6 +53,11 @@ export default class BeaconCache {
         if (lastTimestampForUUID) {
             const diff = Math.abs(moment(lastTimestampForUUID).diff(roundedTimestamp));
             handled = diff < UNIQUE_BEACON_TIMING_IN_MS;
+        }
+
+        if (BLUETOOTH_BEACON_LOGGING === 'verbose') {
+            const short = uuid.substr(0, 8);
+            console.debug(`Handled ${type}/${deviceID}/${short}/${handled}`);
         }
 
         return handled;
