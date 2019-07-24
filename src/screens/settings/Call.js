d238e75030aca4a0ac1650c129810e1860e6f509
Mike Garon <mike@flarejewelry.co>
2019-07-24 14:25:14 -0400

    Style settings/call screen.

--- src/screens/settings/Call.js
+++ src/screens/settings/Call.js
@@ -14,18 +14,22 @@ import Type from '../../bits/Type';
 const styles = StyleSheet.create({
     container: {
         paddingVertical: Spacing.medium,
-        backgroundColor: Colors.backgrounds.blue,
+        backgroundColor: Colors.theme.cream,
         display: 'flex',
         height: '100%',
         justifyContent: 'flex-start',
     },
     title: {
-        paddingVertical: Spacing.medium,
-        paddingHorizontal: Spacing.small,
-        fontSize: Type.size.medium,
+        fontSize: 12,
+        fontWeight: 'bold',
+        letterSpacing: 1.2,
+        color: Colors.black,
+        textTransform: 'uppercase',
+        paddingHorizontal: Spacing.large,
+        marginBottom: Spacing.medium,
     },
     details: {
-        paddingHorizontal: Spacing.small,
+        paddingHorizontal: Spacing.large,
     },
 });
 
@@ -69,7 +73,6 @@ class SettingsCall extends React.PureComponent {
     }
 
     render() {
-        console.log(`Rendering call settings with selected script ${this.state.callScript}`);
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
