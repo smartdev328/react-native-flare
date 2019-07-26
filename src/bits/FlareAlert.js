c16bb5a38e17ef028fd95152e1b269c221ab6738
Mike Garon <mike@flarejewelry.co>
2019-07-26 17:40:39 -0400

    Add the FlareAlert component to display info and warning alerts.

---
+++ src/bits/FlareAlert.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Colors from './Colors';
+import Spacing from './Spacing';
+import Type from './Type';
+
+const styles = StyleSheet.create({
+    container: {
+        margin: Spacing.medium,
+        padding: Spacing.smallish,
+        borderRadius: 7,
+    },
+    warningBackground: {
+        backgroundColor: Colors.red,
+    },
+    infoBackground: {
+        backgroundColor: Colors.purple,
+    },
+    foreground: {
+        color: Colors.white,
+        fontSize: Type.size.small,
+    },
+});
+
+export default function FlareAlert(props) {
+    return (
+        <View style={[styles.container, styles[`${props.variant}Background`]]}>
+            <Text style={styles.foreground}>{props.message}</Text>
+        </View>
+    );
+}
