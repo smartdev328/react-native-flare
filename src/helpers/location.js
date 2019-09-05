199791361c1e167faffc00f032a860b6a0a539a5
Mike Garon <mike@flarejewelry.co>
2019-09-05 10:35:44 -0400

    Refactor location data to match server expectations.

--- src/helpers/location.js
+++ src/helpers/location.js
@@ -1,5 +1,18 @@
 import RNLocation from 'react-native-location';
 
+function parsePosition(position) {
+    const parsed = {
+        coords: {
+            latitude: position.latitude,
+            longitude: position.longitude,
+        },
+        timestamp: position.timestamp,
+        ...position,
+    };
+
+    return parsed;
+}
+
 export default function getCurrentPosition() {
     return new Promise((resolve, reject) => {
         RNLocation.requestPermission({
@@ -8,7 +21,7 @@ export default function getCurrentPosition() {
             if (granted) {
                 RNLocation.configure({ disatnceFilter: 5 });
                 RNLocation.getLatestLocation({ timeout: 60000 }).then(
-                    position => resolve(position),
+                    position => resolve(parsePosition(position)),
                     ({ code, message }) => reject(Object.assign(new Error(message), { name: 'PositionError', code })),
                 );
             }
