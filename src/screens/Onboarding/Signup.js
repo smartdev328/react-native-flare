565cd80f159fb2c668b511e4933183764cfb34e3
mike castleman <m.castleman@touchlab.co>
2019-12-09 16:30:34 -0500

    more of this
    Former-commit-id: 07b9d137736b836f58fb75a855a6fa3bad439b68

--- src/screens/Onboarding/Signup.js
+++ src/screens/Onboarding/Signup.js
@@ -8,7 +8,7 @@ import {
     View,
 } from 'react-native';
 import ViewPager from '@react-native-community/viewpager';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 
 import Colors from '../../bits/Colors';
 import Aura from '../../bits/Aura';
@@ -70,113 +70,137 @@ const extractGreeting = name => {
     }
 };
 
-const Signup = ({ close }) => {
-    const [page, setPage] = React.useState(0);
-    const pagerRef = React.createRef();
-    const fieldRefs = [
-        React.createRef(),
-        React.createRef(),
-        React.createRef(),
-        React.createRef(),
-    ];
-    const name = useSelector(({ user: { reg } }) => reg.name);
-    const greeting = extractGreeting(name);
+class Signup extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            page: 0,
+        };
+        this.fieldRefs = [null, null, null, null];
+        this.setFieldRef = this.fieldRefs.map((_, index) => ref => {
+            this.fieldRefs[index] = ref;
+        });
+    }
 
-    const goBack =
-        page === 0
-            ? close
-            : () => {
-                  setPage(page - 1);
-                  pagerRef.current.setPage(page - 1);
-              };
-
-    const goForward = () => {
-        setPage(page + 1);
-        pagerRef.current.setPage(page + 1);
-    };
-
-    const onPageSelected = ({ nativeEvent: { position } }) => {
-        const field = fieldRefs[position];
-        if (field && field.current) {
-            field.current.focus();
+    goBack = () => {
+        const { close } = this.props;
+        const { page } = this.state;
+        if (page === 0) {
+            close();
+        } else {
+            this.setState({ page: page - 1 });
+            this.pagerRef.setPage(page - 1);
         }
     };
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar barStyle="light-content" />
-            <Aura />
-            <View style={styles.bar}>
-                <TouchableOpacity
-                    style={styles.backArrowWrapper}
-                    onPress={goBack}
-                >
-                    <Image source={backwardArrow} style={styles.backArrow} />
-                </TouchableOpacity>
-                <Image source={smallestWhiteArrow} style={styles.logo} />
-            </View>
-            <ViewPager
-                style={styles.flex}
-                scrollEnabled={false}
-                keyboardDismissMode="none"
-                transitionStyle="scroll"
-                ref={pagerRef}
-                onPageSelected={onPageSelected}
-            >
-                <View key="name">
-                    <FlowScreen
-                        headline="We’re so glad you’re here! What’s your name?"
-                        onNext={goForward}
-                        label="Your name"
-                        textFieldRef={fieldRefs[0]}
-                        textContentType="name"
-                        actionCreator={regSetName}
-                        value="name"
-                        validator={validateName}
-                    />
-                </View>
-                <View key="email">
-                    <FlowScreen
-                        headline={`${greeting} And what’s your email?`}
-                        onNext={goForward}
-                        label="Your email"
-                        textFieldRef={fieldRefs[1]}
-                        keyboardType="email-address"
-                        textContentType="emailAddress"
-                        actionCreator={regSetEmail}
-                        value="email"
-                        validator={validateEmail}
-                    />
-                </View>
-                <View key="phone">
-                    <FlowScreen
-                        headline="And what about your phone number?"
-                        onNext={goForward}
-                        label="Your phone number"
-                        textFieldRef={fieldRefs[2]}
-                        keyboardType="phone-pad"
-                        textContentType="telephoneNumber"
-                        actionCreator={regSetPhone}
-                        value="phone"
-                        validator={validatePhone}
-                    />
-                </View>
-                <View key="password">
-                    <FlowScreen
-                        headline={'Last thing:\nEnter a password!'}
-                        onNext={goForward}
-                        label="Your password"
-                        textFieldRef={fieldRefs[3]}
-                        password
-                        textContentType="password"
-                        actionCreator={regSetPassword}
-                        value="password"
-                        validator={validatePassword}
-                    />
-                </View>
-            </ViewPager>
-        </SafeAreaView>
-    );
-};
+    goForward = () => {
+        const { page } = this.state;
+        this.setState({ page: page + 1 });
+        this.pagerRef.setPage(page + 1);
+    };
 
-export default Signup;
+    onPageSelected = ({ nativeEvent: { position } }) => {
+        const field = this.fieldRefs[position];
+        if (field) {
+            field.focus();
+        }
+    };
+
+    setPagerRef = ref => {
+        this.pagerRef = ref;
+    };
+
+    render() {
+        const { name } = this.props;
+        const greeting = extractGreeting(name);
+
+        return (
+            <SafeAreaView style={styles.container}>
+                <StatusBar barStyle="light-content" />
+                <Aura />
+                <View style={styles.bar}>
+                    <TouchableOpacity
+                        style={styles.backArrowWrapper}
+                        onPress={this.goBack}
+                    >
+                        <Image
+                            source={backwardArrow}
+                            style={styles.backArrow}
+                        />
+                    </TouchableOpacity>
+                    <Image source={smallestWhiteArrow} style={styles.logo} />
+                </View>
+                <ViewPager
+                    style={styles.flex}
+                    scrollEnabled={false}
+                    keyboardDismissMode="none"
+                    transitionStyle="scroll"
+                    ref={this.setPagerRef}
+                    onPageSelected={this.onPageSelected}
+                >
+                    <View key="name">
+                        <FlowScreen
+                            headline="We’re so glad you’re here! What’s your name?"
+                            onNext={this.goForward}
+                            label="Your name"
+                            textFieldRef={this.setFieldRef[0]}
+                            textContentType="name"
+                            actionCreator={regSetName}
+                            value="name"
+                            validator={validateName}
+                        />
+                    </View>
+                    <View key="email">
+                        <FlowScreen
+                            headline={`${greeting} And what’s your email?`}
+                            onNext={this.goForward}
+                            label="Your email"
+                            textFieldRef={this.setFieldRef[1]}
+                            keyboardType="email-address"
+                            textContentType="emailAddress"
+                            actionCreator={regSetEmail}
+                            value="email"
+                            validator={validateEmail}
+                        />
+                    </View>
+                    <View key="phone">
+                        <FlowScreen
+                            headline="And what about your phone number?"
+                            onNext={this.goForward}
+                            label="Your phone number"
+                            textFieldRef={this.setFieldRef[2]}
+                            keyboardType="phone-pad"
+                            textContentType="telephoneNumber"
+                            actionCreator={regSetPhone}
+                            value="phone"
+                            validator={validatePhone}
+                        />
+                    </View>
+                    <View key="password">
+                        <FlowScreen
+                            headline={'Last thing:\nEnter a password!'}
+                            onNext={this.goForward}
+                            label="Your password"
+                            textFieldRef={this.setFieldRef[3]}
+                            password
+                            textContentType="password"
+                            actionCreator={regSetPassword}
+                            value="password"
+                            validator={validatePassword}
+                        />
+                    </View>
+                </ViewPager>
+            </SafeAreaView>
+        );
+    }
+}
+
+const mapStateToProps = ({
+    user: {
+        reg: { name },
+    },
+}) => ({
+    name,
+});
+
+export default connect(mapStateToProps)(Signup);
