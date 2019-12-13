9e83ab641a2414dd50c3c3b8bf684148c0cd2b74
mike castleman <m.castleman@touchlab.co>
2019-12-13 15:10:07 -0500

    SHOP
    Former-commit-id: 58f26352ce0e7561ad672d437f304e873cf55fbb

--- src/screens/Onboarding/Home.js
+++ src/screens/Onboarding/Home.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
-import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+    Linking,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 
 import Voguing from './Voguing';
 import RoundedButton from '../../bits/RoundedButton';
 import Strings from '../../locales/en';
+import Colors from '../../bits/Colors';
 
 import logoWhite from '../../assets/logo-white.png';
 
@@ -13,10 +22,24 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'center',
     },
+    shopLink: {
+        alignSelf: 'flex-end',
+        marginHorizontal: 16,
+        height: 40,
+        marginBottom: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    shopLinkText: {
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+    },
     logo: {
         width: 144,
         height: 62,
-        marginTop: 80,
     },
     spacer: {
         flex: 1,
@@ -29,24 +52,33 @@ const styles = StyleSheet.create({
     },
 });
 
-const Home = ({ onSignUpPressed, onSignInPressed }) => (
-    <SafeAreaView style={styles.wrapper}>
-        <Voguing />
-        <Image source={logoWhite} style={styles.logo} />
-        <View style={styles.spacer} />
-        <RoundedButton
-            useGradient
-            text={Strings.onboarding.signupButton}
-            wrapperStyle={styles.signupButton}
-            onPress={onSignUpPressed}
-        />
-        <RoundedButton
-            useGradient={false}
-            text={Strings.signin.signInLabel}
-            wrapperStyle={styles.signinButton}
-            onPress={onSignInPressed}
-        />
-    </SafeAreaView>
-);
+const Home = ({ onSignUpPressed, onSignInPressed }) => {
+    const openShop = React.useCallback(() => {
+        Linking.openURL('https://getflare.com/');
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.wrapper}>
+            <Voguing />
+            <TouchableOpacity style={styles.shopLink} onPress={openShop}>
+                <Text style={styles.shopLinkText}>Shop</Text>
+            </TouchableOpacity>
+            <Image source={logoWhite} style={styles.logo} />
+            <View style={styles.spacer} />
+            <RoundedButton
+                useGradient
+                text={Strings.onboarding.signupButton}
+                wrapperStyle={styles.signupButton}
+                onPress={onSignUpPressed}
+            />
+            <RoundedButton
+                useGradient={false}
+                text={Strings.signin.signInLabel}
+                wrapperStyle={styles.signinButton}
+                onPress={onSignInPressed}
+            />
+        </SafeAreaView>
+    );
+};
 
 export default Home;
