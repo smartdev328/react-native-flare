8f2aa3c07075e596aafec8c4c79d4134e4956657
mike castleman <m.castleman@touchlab.co>
2019-12-17 15:49:37 -0500

    put link back
    Former-commit-id: 61d62dce559c49ca54103968fca7163ba0052426

--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -41,7 +41,7 @@ const Onboarding = ({ componentId }) => {
     } else {
         return (
             <Home
-                onSignUpPressed={onSignUpSuccess}
+                onSignUpPressed={onSignUpPressed}
                 onSignInPressed={onSignInPressed}
             />
         );
