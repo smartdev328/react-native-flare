392e75f85c9cea987a68364b52af478772ceb4dc
mike castleman <m.castleman@touchlab.co>
2019-12-23 13:13:26 -0500

    rename file
    Former-commit-id: ef4642560fb1453dcc0a2a96a886305b064b1247

--- src/screens/AddHardware/Pairing/index.js
+++ src/screens/AddHardware/Pairing/index.js
@@ -1,1 +1,65 @@
-export default from './Selector';
+import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Bluetooth from './Bluetooth';
+import Confirm from './Confirm';
+import Manual from './Manual';
+import { claimDevice } from '../../../actions';
+import { checkLocationsPermission } from '../../../actions/userActions';
+import { startBleListening } from '../../../actions/hardwareActions';
+
+const Pairing = ({ nextPage, ...props }) => {
+    const dispatch = useDispatch();
+    const { busy, finished, authToken, method, device } = useSelector(
+        ({
+            user: {
+                claimingDevice,
+                claimedDevice,
+                authToken: stateAuthToken,
+                reg: { preferredPairing, foundDevice },
+            },
+        }) => ({
+            busy: claimingDevice,
+            finished: !!claimedDevice,
+            method: preferredPairing,
+            device: foundDevice,
+            authToken: stateAuthToken,
+        })
+    );
+    const submit = React.useCallback(
+        ({ deviceId, secondFactor }) => {
+            if (
+                typeof deviceId !== 'number' ||
+                typeof secondFactor !== 'string' ||
+                secondFactor.length !== 3
+            ) {
+                return;
+            }
+            dispatch(claimDevice(authToken, deviceId, secondFactor));
+        },
+        [dispatch, authToken]
+    );
+
+    React.useEffect(() => {
+        dispatch(startBleListening());
+        dispatch(checkLocationsPermission());
+    }, [dispatch]);
+
+    React.useEffect(() => {
+        if (finished) {
+            nextPage();
+        }
+    }, [finished, nextPage]);
+
+    if (method === 'manual') {
+        return <Manual {...props} busy={busy} submit={submit} />;
+    } else if (device) {
+        return (
+            <Confirm {...props} device={device} busy={busy} submit={submit} />
+        );
+    } else {
+        return <Bluetooth {...props} busy={busy} submit={submit} />;
+    }
+};
+
+export default Pairing;
