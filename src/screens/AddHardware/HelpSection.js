3ecd80a455abf58cb03a20eabcfa8ae52ec819b0
mike castleman <m.castleman@touchlab.co>
2019-12-27 13:47:29 -0500

    How To Connect
    Former-commit-id: 1681698d04640f8a54ddf56690f0fa781bd978d4

--- src/screens/AddHardware/HelpSection.js
+++ src/screens/AddHardware/HelpSection.js
@@ -7,16 +7,20 @@ const sectionStyles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignSelf: 'stretch',
+        marginBottom: 12,
     },
     icon: {
         width: 32,
         height: 32,
         marginRight: 12,
-        alignSelf: 'center',
+        alignSelf: 'flex-start',
     },
     textWrapper: {
         flexDirection: 'column',
         flex: 1,
+    },
+    nomargin: {
+        marginTop: 0,
     },
 });
 
@@ -24,7 +28,15 @@ const HelpSection = ({ icon, title, body }) => (
     <View style={sectionStyles.container}>
         <Image source={icon} style={sectionStyles.icon} resizeMode="center" />
         <View style={sectionStyles.textWrapper}>
-            <Text style={[styles.subhead, styles.whiteText]}>{title}</Text>
+            <Text
+                style={[
+                    styles.subhead,
+                    styles.whiteText,
+                    sectionStyles.nomargin,
+                ]}
+            >
+                {title}
+            </Text>
             <Text style={[styles.helpText, styles.whiteText]}>{body}</Text>
         </View>
     </View>
