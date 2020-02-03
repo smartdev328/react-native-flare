66917a559010ddc4d4f667ecf2f92f739caf9151
mike castleman <m.castleman@touchlab.co>
2020-02-03 15:56:45 -0500

    enable optional-chaining & nullish-coalescing
    Former-commit-id: 63afeac93ac2c9ff5eabbaa2a69289619cf91123

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['@babel/plugin-proposal-unicode-property-regex'],
+    plugins: [
+        '@babel/plugin-proposal-unicode-property-regex',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+    ],
 };
