c7944de024c5dfde2cc2c68ab66760131ca35efc
mike castleman <m.castleman@touchlab.co>
2020-02-03 15:58:05 -0500

    Signup.js: use modern ref style
    Former-commit-id: 21957fd03e6d3d05b3c0391601591ce27d1b9326

--- src/screens/Onboarding/Signup.js
+++ src/screens/Onboarding/Signup.js
@@ -3,6 +3,7 @@ import { Alert, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import ViewPager from '@react-native-community/viewpager';
 import { connect } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import times from 'lodash/times';
 
 import Aura from '../../bits/Aura';
 import FlowScreen from './FlowScreen';
@@ -48,10 +49,8 @@ class Signup extends React.Component {
         this.state = {
             page: 0,
         };
-        this.fieldRefs = [null, null, null, null];
-        this.setFieldRef = this.fieldRefs.map((_, index) => ref => {
-            this.fieldRefs[index] = ref;
-        });
+        this.fieldRefs = times(4, () => React.createRef());
+        this.pagerRef = React.createRef();
     }
 
     componentDidUpdate(prevProps) {
@@ -80,25 +79,18 @@ class Signup extends React.Component {
             close();
         } else {
             this.setState({ page: page - 1 });
-            this.pagerRef.setPage(page - 1);
+            this.pagerRef.current?.setPage(page - 1);
         }
     };
 
     goForward = () => {
         const { page } = this.state;
         this.setState({ page: page + 1 });
-        this.pagerRef.setPage(page + 1);
+        this.pagerRef.current?.setPage(page + 1);
     };
 
     onPageSelected = ({ nativeEvent: { position } }) => {
-        const field = this.fieldRefs[position];
-        if (field) {
-            field.focus();
-        }
-    };
-
-    setPagerRef = ref => {
-        this.pagerRef = ref;
+        this.fieldRefs[position]?.current?.focus();
     };
 
     submit = () => {
@@ -122,7 +114,7 @@ class Signup extends React.Component {
                         scrollEnabled={false}
                         keyboardDismissMode="none"
                         transitionStyle="scroll"
-                        ref={this.setPagerRef}
+                        ref={this.pagerRef}
                         onPageSelected={this.onPageSelected}
                     >
                         <View key="name">
@@ -130,7 +122,7 @@ class Signup extends React.Component {
                                 headline="We’re so glad you’re here! What’s your name?"
                                 onNext={this.goForward}
                                 label="Your full name"
-                                textFieldRef={this.setFieldRef[0]}
+                                textFieldRef={this.fieldRefs[0]}
                                 textContentType="name"
                                 actionCreator={regSetName}
                                 value="name"
@@ -142,7 +134,7 @@ class Signup extends React.Component {
                                 headline={`${greeting} And what’s your email?`}
                                 onNext={this.goForward}
                                 label="Your email"
-                                textFieldRef={this.setFieldRef[1]}
+                                textFieldRef={this.fieldRefs[1]}
                                 keyboardType="email-address"
                                 textContentType="emailAddress"
                                 actionCreator={regSetEmail}
@@ -155,7 +147,7 @@ class Signup extends React.Component {
                                 headline="And what about your phone number?"
                                 onNext={this.goForward}
                                 label="Your phone number"
-                                textFieldRef={this.setFieldRef[2]}
+                                textFieldRef={this.fieldRefs[2]}
                                 keyboardType="phone-pad"
                                 textContentType="telephoneNumber"
                                 actionCreator={regSetPhone}
@@ -168,7 +160,7 @@ class Signup extends React.Component {
                                 headline={'Last thing:\nEnter a password!'}
                                 onNext={this.submit}
                                 label="Your password"
-                                textFieldRef={this.setFieldRef[3]}
+                                textFieldRef={this.fieldRefs[3]}
                                 password
                                 textContentType="password"
                                 actionCreator={regSetPassword}
