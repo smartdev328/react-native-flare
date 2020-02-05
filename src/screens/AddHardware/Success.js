a8bd66b21d3f317d48eb0042a93f368f4a92594c
mike castleman <m.castleman@touchlab.co>
2020-02-05 11:17:40 -0500

    factor out reusable successanimation screen
    Former-commit-id: 8a9a0684afac5a644fb099840ee1c7bf4b36a71f

--- src/screens/AddHardware/Success.js
+++ src/screens/AddHardware/Success.js
@@ -1,37 +1,14 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-import AnimatedLottieView from 'lottie-react-native';
 
-import Colors from '../../bits/Colors';
-import Headline from '../Onboarding/Headline';
-import sharedStyles from './styles';
-
+import SuccessAnimation from '../SuccessAnimation';
 import handshake from '../../assets/lotties/handshake';
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: Colors.theme.cream,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    animation: {
-        width: 400,
-        height: 400,
-    },
-});
-
 const Success = ({ style, nextPage }) => (
-    <View style={[...style, styles.container]}>
-        <Headline style={sharedStyles.headline}>Success!</Headline>
-        <AnimatedLottieView
-            style={styles.animation}
-            source={handshake}
-            autoPlay
-            loop={false}
-            onAnimationFinish={nextPage}
-        />
-    </View>
+    <SuccessAnimation
+        style={style}
+        animation={handshake}
+        onComplete={nextPage}
+    />
 );
 
 export default Success;
