a6ffd2ea34c15d4a1be1fde047935a114e92c0ea
mike castleman <m.castleman@touchlab.co>
2020-02-20 11:29:58 -0500

    allow Aura to fill screen even if screen is big
    Former-commit-id: 3bc4b1e25c97c39c5b753428fdbd6b1aac9056bb

--- src/bits/Aura.js
+++ src/bits/Aura.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
-
-const styles = StyleSheet.create({
-    image: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        resizeMode: 'cover',
-    },
-});
+import { Image } from 'react-native';
+import useDimensions from './useDimensions';
 
 const Aura = ({ source = 'aura-4' }) => {
+    const dims = useDimensions();
     const sourceObj = React.useMemo(
         () => (typeof source === 'string' ? { uri: source } : source),
         [source]
     );
-    return (
-        <View style={StyleSheet.absoluteFill}>
-            <Image source={sourceObj} style={styles.image} />
-        </View>
+    const style = React.useMemo(
+        () => ({
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            width: dims.width,
+            height: dims.height,
+        }),
+        [dims.height, dims.width]
     );
+    return <Image source={sourceObj} style={style} resizeMode="cover" />;
 };
 
 export default Aura;
