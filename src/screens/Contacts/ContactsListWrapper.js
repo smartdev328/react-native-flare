9db1c98b6982b9cf91fc6d28157ae624f6fa46f3
mike castleman <m.castleman@touchlab.co>
2020-02-26 16:25:39 -0500

    search contacts
    Former-commit-id: bb25674b93327d60fc8835b40c863de44170ce67

--- src/screens/Contacts/ContactsListWrapper.js
+++ src/screens/Contacts/ContactsListWrapper.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import {
+    ActivityIndicator,
+    Keyboard,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    View,
+} from 'react-native';
 import { useSelector } from 'react-redux';
 import isArrayLike from 'lodash/isArrayLike';
+import escapeRegExp from 'lodash/escapeRegExp';
+import Icon from 'react-native-vector-icons/Entypo';
 
 import NeedContactsPermission from './NeedContactsPermission';
 import ContactsList from './ContactsList';
+import Colors from '../../bits/Colors';
 
 const styles = StyleSheet.create({
     activityWrapper: {
         flexGrow: 1,
         alignItems: 'center',
         alignContent: 'center',
+    },
+    searchWrapper: {
+        backgroundColor: Colors.backgrounds.blue,
+        alignSelf: 'stretch',
+    },
+    searchInnerWrapper: {
+        marginVertical: 8,
+        marginHorizontal: 16,
+        alignSelf: 'stretch',
+        flexDirection: 'row',
+        backgroundColor: Colors.theme.cream,
+        borderRadius: 8,
+        padding: 12,
+        alignItems: 'center',
+    },
+    icon: {
+        color: Colors.black,
+    },
+    search: {
+        flex: 1,
+        fontSize: 18,
+        lineHeight: 24,
+        marginLeft: 8,
     },
 });
 
@@ -24,9 +58,13 @@ const selector = ({
 });
 
 const ContactsListWrapper = ({ handleContactPress }) => {
+    const searchRef = React.useRef();
     const { contacts, contactsState, contactsCount, loading } = useSelector(
         selector
     );
+
+    const [filter, setFilter] = React.useState('');
+    const usingFilter = filter.trim().length > 0;
 
     const sections = React.useMemo(
         () =>
@@ -35,6 +73,31 @@ const ContactsListWrapper = ({ handleContactPress }) => {
                 : undefined,
         [contacts]
     );
+
+    const filteredContacts = React.useMemo(() => {
+        const re = new RegExp(escapeRegExp(filter.trim()), 'i');
+        if (usingFilter) {
+            return contacts
+                .map(section =>
+                    section.set(
+                        'data',
+                        section.data.filter(({ name }) => re.test(name))
+                    )
+                )
+                .filter(section => section.data.length > 0);
+        } else {
+            return contacts;
+        }
+    }, [contacts, filter, usingFilter]);
+
+    const focusSearch = React.useCallback(() => {
+        searchRef.current?.focus();
+    }, []);
+
+    const clearSearch = React.useCallback(() => {
+        setFilter('');
+        Keyboard.dismiss();
+    }, []);
 
     if (loading || contactsState === 'requested') {
         return (
@@ -46,12 +109,35 @@ const ContactsListWrapper = ({ handleContactPress }) => {
         return <NeedContactsPermission />;
     } else {
         return (
-            <ContactsList
-                contacts={contacts}
-                contactsCount={contactsCount}
-                onPressContact={handleContactPress}
-                sectionList={sections}
-            />
+            <>
+                <TouchableWithoutFeedback
+                    accessibilityRole="none"
+                    onPress={focusSearch}
+                >
+                    <View style={styles.searchWrapper}>
+                        <View style={styles.searchInnerWrapper}>
+                            <Icon name="magnifying-glass" size={24} />
+                            <TextInput
+                                ref={searchRef}
+                                style={styles.search}
+                                placeholder="Search"
+                                onChangeText={setFilter}
+                                keyboardType="default"
+                                value={filter}
+                            />
+                            <TouchableOpacity onPress={clearSearch}>
+                                <Icon name="circle-with-cross" size={24} />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+                <ContactsList
+                    contacts={filteredContacts}
+                    contactsCount={contactsCount}
+                    onPressContact={handleContactPress}
+                    sectionList={usingFilter ? undefined : sections}
+                />
+            </>
         );
     }
 };
