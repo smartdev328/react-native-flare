6febbb80cb073c1e409b3ff06b65a598d903f518
Kevin Schildhorn <kevins@touchlab.co>
2020-03-12 13:09:28 -0400

    Adding option for bracelet ID and categories
    Former-commit-id: 34485c78283ce9abc7044862fe53695172813b46

--- src/actions/LogAction.js
+++ src/actions/LogAction.js
@@ -5,13 +5,9 @@ import {
     checkNotifications,
     PERMISSIONS,
 } from 'react-native-permissions';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import {
-    Permission,
-    PermissionStatus,
-} from 'react-native-permissions/src/types';
 import NetInfo from '@react-native-community/netinfo';
 import { BluetoothStatus } from 'react-native-bluetooth-status';
+import FlareDeviceID from '../bits/FlareDeviceID';
 
 const apiKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS50aW1iZXIuaW8vIiwiZXhwIjpudWxsLCJpYXQiOjE1ODM4NTEzMjYsImlzcyI6Imh0dHBzOi8vYXBpLnRpbWJlci5pby9hcGlfa2V5cyIsInByb3ZpZGVyX2NsYWltcyI6eyJhcGlfa2V5X2lkIjo2ODUzLCJ1c2VyX2lkIjoiYXBpX2tleXw2ODUzIn0sInN1YiI6ImFwaV9rZXl8Njg1MyJ9.fZ3YCnCQ8v3_yTQvNe9WaT0DRkEfFxyY07Pe6YgjSII';
@@ -19,64 +15,61 @@ const sourceId = '34447';
 let logger;
 let usernameStr = '';
 
-const generateMetaData = new Promise(resolve => {
-    Promise.all([
-        check(PERMISSIONS.IOS.CONTACTS),
-        check(PERMISSIONS.IOS.LOCATION_ALWAYS),
-        checkNotifications(),
-        NetInfo.fetch('cellular'),
-        NetInfo.fetch('wifi'),
-        BluetoothStatus.state(),
-    ]).then(
-        ([
-            contactsPermissionStatus,
-            locationPermissionStatus,
-            notificationStatus,
-            cellularState,
-            wifiState,
-            bluetoothState,
-        ]) => {
-            resolve({
-                phone_state: {
-                    permissions: {
-                        location: locationPermissionStatus,
-                        contacts: contactsPermissionStatus,
-                        notifications: notificationStatus.status,
-                        bluetooth: bluetoothState ? 'enabled' : 'disabled',
+function generateMetaData(deviceSerialNum): Promise {
+    return new Promise(resolve => {
+        Promise.all([
+            check(PERMISSIONS.IOS.CONTACTS),
+            check(PERMISSIONS.IOS.LOCATION_ALWAYS),
+            checkNotifications(),
+            NetInfo.fetch('cellular'),
+            NetInfo.fetch('wifi'),
+            BluetoothStatus.state(),
+        ]).then(
+            ([
+                contactsPermissionStatus,
+                locationPermissionStatus,
+                notificationStatus,
+                cellularState,
+                wifiState,
+                bluetoothState,
+            ]) => {
+                resolve({
+                    phone_state: {
+                        permissions: {
+                            location: locationPermissionStatus,
+                            contacts: contactsPermissionStatus,
+                            notifications: notificationStatus.status,
+                            bluetooth: bluetoothState ? 'enabled' : 'disabled',
+                        },
+                        connectivity: {
+                            wifi: wifiState.isConnected
+                                ? 'connected'
+                                : 'disconnected',
+                            cellular: cellularState.isConnected
+                                ? 'connected'
+                                : 'disconnected',
+                            bluetooth: bluetoothState ? 'enabled' : 'disabled',
+                        },
                     },
-                    connectivity: {
-                        wifi: wifiState.isConnected
-                            ? 'connected'
-                            : 'disconnected',
-                        cellular: cellularState.isConnected
-                            ? 'connected'
-                            : 'disconnected',
-                        bluetooth: bluetoothState ? 'enabled' : 'disabled',
+                    user_info: {
+                        bracelet_serial_number: deviceSerialNum,
+                        user_id: usernameStr,
                     },
-                },
-                user_info: {
-                    bracelet_serial_number: '',
-                    user_id: usernameStr,
-                },
-                timestamp: new Date().toString(),
-            });
-        }
-    );
-});
+                    timestamp: new Date().toString(),
+                });
+            }
+        );
+    });
+}
+
+export const FlareLoggerCategory = {
+    wake: 'WAKE',
+    button: 'BUTTON',
+    send: 'API_SEND',
+    receive: 'API_RECEIVE',
+};
 
 export class FlareLogger {
-    static async addCurrentUser(
-        username,
-        log: ITimberLog
-    ): Promise<ITimberLog> {
-        return {
-            ...log,
-            user: {
-                name: username,
-            },
-        };
-    }
-
     static initLogging() {
         logger = new Timber(apiKey, sourceId);
         async function addDeviceInfo(log: ITimberLog): Promise<ITimberLog> {
@@ -93,74 +86,52 @@ export class FlareLogger {
         }
         logger.use(addDeviceInfo);
     }
-    /*
-    static generateMetaData() : String {
-
-        Promise.all([
-            check(PERMISSIONS.IOS.CONTACTS),
-            check(PERMISSIONS.IOS.LOCATION_ALWAYS),
-            checkNotifications(),
-            NetInfo.fetch("cellular"),
-            NetInfo.fetch('wifi'),
-            BluetoothStatus.state(),
-        ]).then(([contactsPermissionStatus, locationPermissionStatus, notificationStatus, cellularState, wifiState, bluetoothState]) => {
-            return {
-                phone_state: {
-                    permissions: {
-                        location: locationPermissionStatus,
-                        contacts: contactsPermissionStatus,
-                        notifications: notificationStatus.status,
-                        bluetooth: (bluetoothState ? 'enabled' : 'disabled'),
-                    },
-                    connectivity: {
-                        wifi: (wifiState.isConnected ? 'connected' : 'disconnected'),
-                        cellular: (cellularState.isConnected ? 'connected' : 'disconnected'),
-                        bluetooth: (bluetoothState ? 'enabled' : 'disabled'),
-                    }
-                },
-                user_info: {
-                    bracelet_serial_number: '',
-                    user_id: usernameStr,
-                },
-                timestamp: new Date().toString(),
-            };
-        });
-    }*/
 
     static setLoginInfo(username) {
         usernameStr = username;
-        logger.use(FlareLogger.addCurrentUser(username));
     }
 
     static removeLoginInfo() {
-        logger.remove(FlareLogger.addCurrentUser);
+        usernameStr = '';
     }
 
-    static error(logString) {
-        generateMetaData.then(metaData => {
+    static error(categoryStr, logString, optionalBraceletId) {
+        const braceletId = optionalBraceletId
+            ? FlareDeviceID.getJewelryLabelFromDeviceID(optionalBraceletId)
+            : '';
+        generateMetaData(braceletId).then(metaData => {
             console.debug('Metadata', metaData);
-            logger.error(logString, metaData);
+            logger.error(logString, { category: categoryStr }, metaData);
         });
     }
 
-    static warn(logString) {
-        generateMetaData.then(metaData => {
+    static warn(categoryStr, logString, optionalBraceletId) {
+        const braceletId = optionalBraceletId
+            ? FlareDeviceID.getJewelryLabelFromDeviceID(optionalBraceletId)
+            : '';
+        generateMetaData(braceletId).then(metaData => {
             console.debug('Metadata', metaData);
-            logger.warn(logString, metaData);
+            logger.warn(logString, { category: categoryStr }, metaData);
         });
     }
 
-    static debug(logString) {
-        generateMetaData.then(metaData => {
+    static debug(categoryStr, logString, optionalBraceletId) {
+        const braceletId = optionalBraceletId
+            ? FlareDeviceID.getJewelryLabelFromDeviceID(optionalBraceletId)
+            : '';
+        generateMetaData(braceletId).then(metaData => {
             console.debug('Metadata', metaData);
-            logger.debug(logString, metaData);
+            logger.debug(logString, { category: categoryStr }, metaData);
         });
     }
 
-    static info(logString) {
-        generateMetaData.then(metaData => {
+    static info(categoryStr, logString, optionalBraceletId) {
+        const braceletId = optionalBraceletId
+            ? FlareDeviceID.getJewelryLabelFromDeviceID(optionalBraceletId)
+            : '';
+        generateMetaData(braceletId).then(metaData => {
             console.debug('Metadata', metaData);
-            logger.info(logString, metaData);
+            logger.info(logString, { category: categoryStr }, metaData);
         });
     }
 }
