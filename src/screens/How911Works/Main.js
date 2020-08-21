589892ce827c60ecabd713a2c21ef2d18712e74d
Shemar Anderson <toptal@toptals-iMac.local>
2020-08-21 07:05:18 -0400

    Update main page

---
+++ src/screens/How911Works/Main.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import {
+    Image,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import { Navigation } from 'react-native-navigation';
+
+import Colors from '../../bits/Colors';
+import WhiteBar from '../Onboarding/WhiteBar';
+import Headline from '../Onboarding/Headline';
+import RoundedButton from '../../bits/RoundedButton';
+
+import StarryLocationImg from '../../assets/starry-location.png';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: Colors.theme.cream,
+        alignItems: 'center',
+    },
+    shrink: {
+        height: 10,
+        flexShrink: 1,
+    },
+    headline: {
+        alignSelf: 'center',
+        textAlign: 'center',
+        color: Colors.black,
+        marginBottom: 0,
+    },
+    line: {
+        height: 1,
+        width: 33,
+        marginVertical: 12,
+        backgroundColor: Colors.black,
+    },
+    subhead: {
+        width: 307,
+        color: Colors.black,
+        fontSize: 18,
+        lineHeight: 24,
+        fontFamily: 'Nocturno Display Std',
+        textAlign: 'center',
+        marginBottom: 60,
+    },
+    button: {
+        marginTop: 16,
+        marginBottom: 24,
+        position: 'absolute',
+        bottom: 64,
+    },
+    starryImg: {
+        width: 240,
+        resizeMode: 'contain',
+    },
+});
+
+const How911Works = ({ componentId }) => {
+    const close = React.useCallback(() => {
+        Navigation.pop(componentId);
+    }, [componentId]);
+
+    const moveNext = React.useCallback(() => {
+        Navigation.push(componentId, {
+            component: {
+                name: 'com.flarejewelry.how911works.textandcall',
+                options: {
+                    topBar: {
+                        visible: false,
+                        animate: false,
+                    },
+                },
+            },
+        });
+    }, [componentId]);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <WhiteBar showBack showLogo={false} black goBack={close} />
+            <Headline style={styles.headline}>
+                How Flare Works With 911
+            </Headline>
+            <View style={styles.shrink} />
+            <View style={styles.line} />
+            <Text style={styles.subhead}>
+                Flare will send your GPS location to 911 and your Crew - even if
+                you don't know where you are or can't speak.{'\n'}The dispatcher
+                will be able to view your live location on their screen.
+            </Text>
+            <Image style={styles.starryImg} source={StarryLocationImg} />
+            <RoundedButton
+                onPress={moveNext}
+                wrapperStyle={styles.button}
+                text="NEXT"
+                width={240}
+                fontSize={12}
+            />
+        </SafeAreaView>
+    );
+};
+
+export default How911Works;
